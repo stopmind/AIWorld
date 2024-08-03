b7e36fe084b3d398ce17042c137952dd8b544574
stopmind <smaxim120@gmail.com>
2024-08-03 23:02:59 +0300

    Initial

---
+++ AIWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AIWorld.Scenes;
+using Teko.Utils;
+new GameBuilder()
+    .StdServices(["Assets"], "game.log")
+    .Window(1200, 675, "AI World")
+    .Scene(new BootstrapScene())
+    .Run();
---
+++ AIWorld/Scenes/BootstrapScene.cs
@@ -0,0 +1,26 @@
+﻿using Teko.Core;
+
+namespace AIWorld.Scenes;
+
+public class BootstrapScene : Scene
+{
+    public override void Ready()
+    {
+        
+    }
+
+    public override void Update(float delta)
+    {
+        
+    }
+
+    public override void Draw(float delta)
+    {
+        
+    }
+
+    public override void OnClose()
+    {
+        Game.Exit();
+    }
+}
