91451ade837f3f01c7578836c853ec684677abb4
stopmind <smaxim120@gmail.com>
2024-08-08 17:28:28 +0300

    Camera

---
+++ AIWorld/World/Camera.cs
@@ -0,0 +1,33 @@
+﻿using Teko.Core;
+using Teko.Graphics;
+using Teko.Input;
+
+namespace AIWorld.World;
+
+public class Camera
+{
+    private const float MoveSpeed = 300f;
+    private const float ZoomSpeed = 0.8f;
+    private const float ZoomMin = 0.4f;
+    private const float ZoomMax = 4f;
+    
+    private Vector2f _position = Vector2f.Zero;
+    private float _zoom = 2;
+
+    public void Update(GraphicsService graphics, Input input, float delta)
+    {
+        var move = Vector2f.Zero;
+        if (input.IsDown("camera_up")) move += Vector2f.Up;
+        if (input.IsDown("camera_down")) move += Vector2f.Down;
+        if (input.IsDown("camera_left")) move += Vector2f.Left;
+        if (input.IsDown("camera_right")) move += Vector2f.Right;
+        _position += move.Normalize() * MoveSpeed / _zoom * delta;
+
+        var zoomChange = 0f;
+        if (input.IsDown("camera_zoom"))   zoomChange++;
+        if (input.IsDown("camera_unzoom")) zoomChange--;
+        _zoom = MathF.Min(ZoomMax, MathF.Max(ZoomMin, _zoom + zoomChange * delta * ZoomSpeed));
+        
+        graphics.SetView(0, new View(_position, graphics.GetSize().ToFloat() / _zoom));
+    }
+}
--- AIWorld/World/WorldScene.cs
+++ AIWorld/World/WorldScene.cs
@@ -1,5 +1,6 @@
 ﻿using Teko.Core;
 using Teko.Graphics;
+using Teko.Input;
 using Teko.Resources;
 
 namespace AIWorld.World;
@@ -7,10 +8,21 @@ namespace AIWorld.World;
 public class WorldScene : Scene
 {
     private GraphicsService? _graphics;
+    private Input? _input;
     private Chunk? _chunk;
+    private readonly Camera _camera = new();
     
     public override void Ready()
     {
+        _input = Game.GetService<Input>();
+        _input.SetKeyboardEvent("camera_up", Key.W);
+        _input.SetKeyboardEvent("camera_down", Key.S);
+        _input.SetKeyboardEvent("camera_left", Key.A);
+        _input.SetKeyboardEvent("camera_right", Key.D);
+        
+        _input.SetKeyboardEvent("camera_zoom", Key.O);
+        _input.SetKeyboardEvent("camera_unzoom", Key.P);
+        
         _graphics = Game.GetService<GraphicsService>();
         _graphics.FillColor = new Color(0x1c1c1fFF);
         _chunk = new Chunk(Vector2f.Zero,
@@ -19,7 +31,7 @@ public class WorldScene : Scene
 
     public override void Update(float delta)
     {
-        
+        _camera.Update(_graphics!, _input!, delta);
     }
 
     public override void Draw(float delta)
